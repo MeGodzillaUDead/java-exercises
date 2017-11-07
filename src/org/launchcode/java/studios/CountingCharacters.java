b7337f9df244f04fdac1a90c69567b6605011ef4
Jonathan Riezman <riezman@gmail.com>
2017-11-06 18:51:55 -0600

    Changed from hard-coded string to getting user input.

--- src/org/launchcode/java/studios/CountingCharacters.java
+++ src/org/launchcode/java/studios/CountingCharacters.java
@@ -2,10 +2,14 @@ package org.launchcode.java.studios;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class CountingCharacters {
     public static void main(String[] args){
-        String stringToCount = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a string to get a character count: ");
+        String stringToCount = in.nextLine();
+
         String noCase = stringToCount.toLowerCase(); //to make it case-insensitive in a crude way
         char[] charsInString = noCase.toCharArray();
 
