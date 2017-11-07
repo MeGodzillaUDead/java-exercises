05204d501e2c180f5d66ba231b22f033247a2ff2
Jonathan Riezman <riezman@gmail.com>
2017-11-06 18:43:59 -0600

    Made CountingCharacters case-insensitive

--- src/org/launchcode/java/studios/CountingCharacters.java
+++ src/org/launchcode/java/studios/CountingCharacters.java
@@ -6,14 +6,17 @@ import java.util.Map;
 public class CountingCharacters {
     public static void main(String[] args){
         String stringToCount = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
-        char[] charsInString = stringToCount.toCharArray();
+        String noCase = stringToCount.toLowerCase(); //to make it case-insensitive in a crude way
+        char[] charsInString = noCase.toCharArray();
 
         HashMap<Character, Integer> letterCount = new HashMap<>();
 
         for (char each : charsInString){
             if (letterCount.containsKey(each)){
+                //if letter is already in map increment the count
                 letterCount.put(each, letterCount.get(each) + 1);
             } else {
+                //if letter is not already in map add it
                 letterCount.put(each, 1);
             }
         }
